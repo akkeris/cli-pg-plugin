ccae5de5bf6d96385f7745aa8098a9417c5e23ba
Trevor Linton <trevor.linton@gmail.com>
2018-11-09 08:09:04 -0700

    COBRA-2020 - fix the UX when taking backups

--- commands/backups.js
+++ commands/backups.js
@@ -13,6 +13,10 @@ async function backup_list (appkit, args) {
   }
 }
 
+async function wait(timeInMills) {
+  return new Promise((resolve, reject) => setTimeout(resolve, timeInMills))
+}
+
 async function backup_capture (appkit, args) {
   try {
     let pg = await common.find(appkit, args.app, args.database)
@@ -24,15 +28,54 @@ async function backup_capture (appkit, args) {
 }
 
 async function backup_restore (appkit, args) {  
+  let maintenance_ran = false;
+  let loader = null
   try {
     assert.ok(args.BACKUP && args.BACKUP !== '', 'A backup to restore was not specified.')
     let pg = await common.find(appkit, args.app, args.database)
+    if(args.confirm !== args.BACKUP) {
+      let confirm = await new Promise((resolve, reject) => { appkit.terminal.confirm(`\n ~~▸~~    WARNING: This will restore ${args.BACKUP} on database **+ ${pg.name}** attached to ${args.app}.\n ~~▸~~    Any app attached to it will experience downtime during the restore\n ~~▸~~    To proceed, type !!${args.BACKUP}!! or re-run this command with !!--confirm ${args.BACKUP}!!\n`, (a) => resolve(a)) });
+      if(confirm !== args.BACKUP) {
+        appkit.terminal.soft_error(`Confirmation did not match !!${args.BACKUP}!!. Retore aborted.`);
+        return
+      }
+    }
     let action = pg.addon_service.name === 'akkeris-postgresql' ? `backups/${args.BACKUP}` : 'backups-restore'
     let payload = pg.addon_service.name === 'akkeris-postgresql' ? null : JSON.stringify({"backup":args.BACKUP})
     let method = pg.addon_service.name === 'akkeris-postgresql' ? appkit.api.put : appkit.api.post
-    appkit.terminal.vtable(await method(payload, `/apps/${args.app}/addons/${pg.id}/actions/${action}`))
+
+    loader = appkit.terminal.loading(appkit.terminal.markdown(`\n###===### Placing app ~~${args.app}~~ into maintenance mode.`));
+    loader.start()
+    await appkit.api.patch(JSON.stringify({"maintenance":true}), `/apps/${args.app}`)
+    maintenance_ran = true
+    loader.end()
+    loader = appkit.terminal.loading(appkit.terminal.markdown(`\n###===### Starting restore on ~~${args.app}~~ for ~~${pg.name}~~`));
+    loader.start()
+    await method(payload, `/apps/${args.app}/addons/${pg.id}/actions/${action}`)
+    loader.end()
+    loader = appkit.terminal.loading(appkit.terminal.markdown(`\n###===### Waiting for database to be restored on ~~${args.app}~~ for ~~${pg.name}~~ (this may take 15~30 minutes)`));
+    loader.start()
+    for (let i=0; i < 100; i++) {
+      if(i === 99) {
+        throw new Error('The database has not yet restored, it may still be in progress or an error may have occured. Please contact your local maytag man.')
+      }
+      await wait(5000) // addon status doesnt update (for rate limiting reasons) for every 30 seconds, so no point in lowering this.
+      let addon = await appkit.api.get(`/apps/${args.app}/addons/${pg.name}`)
+      if(addon.state !== 'provisioning') {
+        break;
+      }
+    }
+    loader.end();
+    console.log(appkit.terminal.markdown(`###===### Database ~~${pg.name}~~ was successfully restored to ${args.BACKUP}.\n`));
   } catch (e) {
+    if(loader) {
+      loader.end()
+    }
     return appkit.terminal.error(e)
+  } finally {
+    if(maintenance_ran) {
+      await appkit.api.patch(JSON.stringify({"maintenance":false}), `/apps/${args.app}`)
+    }
   }
 }
 
@@ -51,6 +94,13 @@ module.exports = {
         'string':true,
         'description':'The name of the postgres addon to use'
       }
+    }
+    let require_restore_options = JSON.parse(JSON.stringify(require_options))
+    require_restore_options.confirm = {
+      'alias':'c',
+      'demand':false,
+      'string':true,
+      'description':'Confirm the action via the command line, the value should be the backup name'
     }
 
     appkit.args
