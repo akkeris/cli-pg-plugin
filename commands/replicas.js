0244b03749f6746fd06ee2120c57e3570fbb9445
cachristopher <cory.christopher@octanner.com>
2019-08-02 11:59:17 -0600

    cleaned up the cli

---
+++ commands/replicas.js
@@ -0,0 +1,67 @@
+
+const assert = require('assert')
+const common = require('../lib/common')
+
+async function replica_list (appkit, args) {
+  try {
+    let pg = await common.find(appkit, args.app, args.database)
+    let call = pg.addon_service.name === 'akkeris-postgresql' ? appkit.api.get : appkit.api.post.bind(null, null)
+    let data = await appkit.api.get(`/apps/${args.app}/addons/${pg.id}/actions/replica`)
+    appkit.terminal.table(data)
+  } catch (e) {
+    return appkit.terminal.error(e)
+  }
+}
+
+async function replica_create (appkit, args) {
+  try {
+    let pg = await common.find(appkit, args.app, args.database)
+    let action = pg.addon_service.name === 'akkeris-postgresql' ? 'replica' : 'replica-create'
+    let data = await appkit.api.put(null, `/apps/${args.app}/addons/${pg.id}/actions/${action}`)
+    delete data.Plan
+    appkit.terminal.vtable(data)
+  } catch (err) {
+    appkit.terminal.error(err);
+  }
+}
+
+async function replica_destroy (appkit, args) {
+  try {
+    assert.ok(args.REPLICA && args.REPLICA !== '', 'No replica was provided to destroy.')
+    let pg = await common.find(appkit, args.app, args.database)
+    let action = pg.addon_service.name === 'akkeris-postgresql' ? 'replica' : 'replica-destroy'
+    let data = await appkit.api.delete(null, `/apps/${args.app}/addons/${pg.id}/actions/${action}`)
+    delete data.Plan
+    appkit.terminal.vtable(data)
+  } catch (err) {
+    appkit.terminal.error(err);
+  }
+}
+
+module.exports = {
+  init:function (appkit) {
+    let require_options = {
+      'app':{
+        'alias':'a',
+        'demand':false,
+        'string':true,
+        'description':'The app to act on.'
+      },
+      'database':{
+        'alias':'d',
+        'demand':false,
+        'string':true,
+        'description':'The name of the postgres addon to use'
+      }
+    }
+
+    appkit.args
+      .command('pg:replicas', 'list database read replicas', require_options, replica_list.bind(null, appkit))
+      .command('pg:replicas:create', 'create a new database read replica', require_options, replica_create.bind(null, appkit))
+      .command('pg:replicas:destroy', 'destroy a database read replica', require_options, replica_destroy.bind(null, appkit))
+  },
+  update:function() {},
+  'group':'pg',
+  'help':'manage postgres databases',
+  'primary':false
+}
